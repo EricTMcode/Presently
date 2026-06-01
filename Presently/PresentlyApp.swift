7513cb68758d0754bab7589748535c36160dbdf6
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-06-01 14:06:06 +0200

    Initial Commit

---
+++ Presently/PresentlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentlyApp.swift
+//  Presently
+//
+//  Created by Eric on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct PresentlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
