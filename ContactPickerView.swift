723bd3d299ae06283e9c1fba9c57de5518d0a1fa
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-06-01 15:24:28 +0200

    Create ContactPickerView

---
+++ ContactPickerView.swift
@@ -0,0 +1,13 @@
+//
+//  ContactPickerView.swift
+//  Presently
+//
+//  Created by Eric on 01/06/2026.
+//
+
+import ContactsUI
+import SwiftUI
+
+struct ContactPickerView: UIViewControllerRepresentable {
+    
+}
