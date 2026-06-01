15fe1494fa42830e13f60db30be7eb039f7c309e
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-06-01 15:15:07 +0200

    Create BirthdayStore

---
+++ Presently/BirthdayStore.swift
@@ -0,0 +1,13 @@
+//
+//  BirthdayStore.swift
+//  Presently
+//
+//  Created by Eric on 01/06/2026.
+//
+
+import Foundation
+
+@Observable @MainActor
+class BirthdayStore {
+    var contacts = [BirthdayContact]()
+}
